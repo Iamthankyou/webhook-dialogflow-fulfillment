cdfc768232141bc587e1468e732f4d315144500f
Le Quang Duy <quangduynb00@gmail.com>
2021-08-16 17:46:55 +0700

    INIT

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({request, response})
 
     function sayHello(agent){
-        agent.add("Hello, this was a nice tutorial by axlewebtech")
+        agent.add("Hello, this was a nice ")
     }
 
     let intentMap = new Map();
